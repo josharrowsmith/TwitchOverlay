5997fdfb4c1826e4dba0d762c7292f6977ba5921
josh arrowsmith <josh.arrowsmith@hotmail.com>
2020-10-30 12:22:43 +1000

    removed rooms functions

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { setToken, isLoggedIn, getStream } from "../../util"
-import { useImmer } from 'use-immer';
 // this is fucked but i can't be bother to fix it yet
 import 'regenerator-runtime/runtime';
 import useSocket from 'use-socket.io-client';
@@ -8,10 +7,6 @@ import BallScrene from "../BallScene/BallScrene"
 import "./login.css"
 
 const twitch = window.Twitch.ext;
-
-const Messages = props => props.data.map(m => m[0] !== '' ? (<li><strong>{m[0]}</strong> : <div className="innermsg">{m[1]}</div></li>) : (<li className="update">{m[1]}</li>));
-
-const Online = props => props.data.map(m => <li id={m[0]}>{m[1]}</li>)
 
 export default () => {
     // Server stuff
@@ -24,47 +19,16 @@ export default () => {
     const [loggedIn, setLoggedIn] = useState('');
     const [loading, setLoading] = useState(false)
 
-    const [messages, setMessages] = useImmer([]);
-    const [online, setOnline] = useImmer([]);
-
-
     // Server stuff
     const [socket] = useSocket('https://grandmasternightfalls.herokuapp.com/');
     socket.connect();
 
     useEffect(() => {
-
-        // For testing
-        socket.on('update', message => setMessages(draft => {
-            draft.push(['', message]);
-        }))
-
-        socket.on('people-list', people => {
-            let newState = [];
-            for (let person in people) {
-                newState.push([people[person].id, people[person].name]);
-            }
-            setOnline(draft => { draft.push(...newState) });
-            console.log(online)
-        });
-
-        socket.on('add-person', (name, id) => {
-            setOnline(draft => {
-                draft.push([id, name])
-            })
-        })
-
-        socket.on('remove-person', id => {
-            setOnline(draft => draft.filter(m => m[0] !== id))
-        })
-
         // Main results function
         socket.on('FromAPI', (data) => {
             setResults(data)
             setLoading(true)
         });
-
-      
     }, []);
 
     // Streamer entry
